4668e6058d0d5ea817d79cd4eb2c551c4e16b7e1
Yury-rusakov <yury.wtf@yandex.ru>
2026-03-17 23:26:12 +0300

    Added HomeActivity and Calculator layout
    Реализовал переходы между экранами

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,14 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    val nav_version = "2.7.7" // Проверьте актуальную версию
+
+    // Навигация
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/topacademy_android/MainActivity.kt
+++ app/src/main/java/com/example/topacademy_android/MainActivity.kt
@@ -1,68 +1,23 @@
 package com.example.topacademy_android
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.topacademy_android.databinding.ActivityMainBinding
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
-
-    companion object{
-        private const val ON_CREATE = "ON_CREATE"
-    }
-
     private lateinit var binding: ActivityMainBinding
-
-    private var startTime: Long = 0
-    private val TAG = "Lifecycle"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startTime = System.currentTimeMillis()
-        Log.d(TAG, "onCreate вызван")
-
-        binding.button.setOnClickListener {
-            binding.twTitle.text = getString(R.string.creatorName)
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onStart вызван через $diff мс после onCreate")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onResume вызван через $diff мс после onCreate")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onPause вызван через $diff мс после onCreate")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onStop вызван через $diff мс после onCreate")
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onRestart вызван через $diff мс после onCreate")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        val diff = System.currentTimeMillis() - startTime
-        Log.d(TAG, "onDestroy вызван через $diff мс после onCreate")
     }
 }
